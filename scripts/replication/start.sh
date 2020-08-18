29dfcc56a7595d65e820ac564f2cbcf6b7042910
Igor Gaponenko <gapon@lsst-qserv-master03.ncsa.illinois.edu>
2020-08-17 22:06:32 -0500

    The initial commit of the management scripts for the "small" cluster.

---
+++ scripts/replication/start.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# This file is part of qserv.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Start services on all nodes
+
+set -e
+set -x
+# Load parameters of the setup into the corresponding environment
+# variables
+
+. $(dirname "$0")/env_svc.sh
+
+# Start database services on the master node
+#
+# The general log is temporarily disabled because it results in a huge size
+# of the log file.
+#
+#   --general-log --general-log-file="${DB_DATA_DIR}/log/${DB_CONTAINER_NAME}.general.log" \
+
+if [ -n "${DB_SERVICE}" ]; then
+    HOST="qserv-${MASTER}"
+    echo "[${MASTER}] starting database service"
+    ssh -n $HOST docker run \
+        --detach \
+        --name "${DB_CONTAINER_NAME}" \
+        -u ${CONTAINER_UID}:${CONTAINER_GID} \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v "${DB_DATA_DIR}/mysql:/var/lib/mysql" \
+        -v "${DB_DATA_DIR}/log:${DB_DATA_DIR}/log" \
+        -v "${DB_DATA_DIR}/config/docker/etc/mysql/conf.d/docker.cnf:/etc/mysql/conf.d/docker.cnf:ro" \
+        -e "MYSQL_ROOT_PASSWORD=${DB_ROOT_PASSWORD}" \
+        -p "${DB_PORT}:${DB_PORT}/tcp" \
+        "${DB_IMAGE_TAG}" \
+        --port="${DB_PORT}" \
+        --max-connections=16384 \
+        --query-cache-size=0 \
+        --log-error="${DB_DATA_DIR}/log/${DB_CONTAINER_NAME}.error.log" \
+        --slow-query-log --slow-query-log-file="${DB_DATA_DIR}/log/${DB_CONTAINER_NAME}.slow-query.log" \
+        --log-warnings=2 \
+        --pid-file="${DB_DATA_DIR}/log/${DB_CONTAINER_NAME}.pid" &
+fi
+
+# Start workers on all nodes
+#
+# ATTENTION: limit memory size of the containers by 40 GB due to a known memory
+# management bug in the implementation of the worker services.
+
+for WORKER in $WORKERS; do
+    HOST="qserv-${WORKER}"
+    echo "[${WORKER}] starting worker agent"
+    ssh -n $HOST docker run \
+        --detach \
+        --network host \
+        --memory 42949672960 \
+        --name "${WORKER_CONTAINER_NAME}" \
+        -u ${CONTAINER_UID}:${CONTAINER_GID} \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v "${QSERV_DATA_DIR}:/qserv/data" \
+        -v "${QSERV_DATA_DIR}/export:/qserv/data/export" \
+        -v "${CONFIG_DIR}:/qserv/replication/config:ro" \
+        -v "${LOG_DIR}:/qserv/replication/log" \
+        -e "WORKER_CONTAINER_NAME=${WORKER_CONTAINER_NAME}" \
+        -e "LSST_LOG_CONFIG=/qserv/replication/config/${LOG_CONFIG}" \
+        -e "CONFIG=${CONFIG}" \
+        -e "INSTANCE_ID=${INSTANCE_ID}" \
+        -e "WORKER=${WORKER}" \
+        -e "QSERV_WORKER_DB_PASSWORD=${QSERV_WORKER_DB_PASSWORD}" \
+        -e "AUTH_KEY=${AUTH_KEY}" \
+       "${REPLICATION_IMAGE_TAG}" \
+        bash -c \''lsst qserv-replica-worker ${WORKER} --config=${CONFIG} --instance-id=${INSTANCE_ID} --qserv-db-password="${QSERV_WORKER_DB_PASSWORD}" --auth-key="${AUTH_KEY}" --debug >& /qserv/replication/log/${WORKER_CONTAINER_NAME}.log'\' &
+done
+
+# Start master controller
+
+if [ -n "${MASTER_CONTROLLER}" ]; then
+    HOST="qserv-${MASTER}"
+    echo "[${MASTER}] starting master controller"
+    OPT_MALLOC_CONF=
+    OPT_LD_PRELOAD=
+    if [ ! -z "${USE_JEMALLOC}" ]; then
+        OPT_MALLOC_CONF=prof_leak:true,lg_prof_interval:31,lg_prof_sample:22,prof_final:true
+        OPT_LD_PRELOAD=/qserv/lib/libjemalloc.so
+    fi
+    ssh -n $HOST docker run \
+        --detach \
+        --network host \
+        --name "${MASTER_CONTAINER_NAME}" \
+        -u ${CONTAINER_UID}:${CONTAINER_GID} \
+        -v "/etc/passwd:/etc/passwd:ro" \
+        -v "${WORK_DIR}:/qserv/replication/work" \
+        -v "${QSERV_DATA_DIR}/qserv:/qserv/data/qserv" \
+        -v "${QSERV_DATA_DIR}/ingest:/qserv/data/ingest" \
+        -v "${CONFIG_DIR}:/qserv/replication/config:ro" \
+        -v "${LOG_DIR}:/qserv/replication/log" \
+        -e "MASTER_CONTAINER_NAME=${MASTER_CONTAINER_NAME}" \
+        -e "'PARAMETERS=${MASTER_PARAMETERS}'" \
+        -e "LSST_LOG_CONFIG=/qserv/replication/config/${LOG_CONFIG}" \
+        -e "CONFIG=${CONFIG}" \
+        -e "INSTANCE_ID=${INSTANCE_ID}" \
+        -e "OPT_MALLOC_CONF=${OPT_MALLOC_CONF}" \
+        -e "OPT_LD_PRELOAD=${OPT_LD_PRELOAD}" \
+        -e "QSERV_MASTER_DB_PASSWORD=${QSERV_MASTER_DB_PASSWORD}" \
+        -e "AUTH_KEY=${AUTH_KEY}" \
+        "${REPLICATION_IMAGE_TAG}" \
+        bash -c \''lsst qserv-replica-master-http ${PARAMETERS} --config=${CONFIG} --instance-id=${INSTANCE_ID} --qserv-db-password="${QSERV_MASTER_DB_PASSWORD}" --auth-key="${AUTH_KEY}" --debug >& /qserv/replication/log/${MASTER_CONTAINER_NAME}.log'\' &
+fi
+wait
