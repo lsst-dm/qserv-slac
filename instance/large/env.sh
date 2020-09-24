48f716b6380d39dcfb6366d589dc1b781b219069
Igor Gaponenko <gapon@slac.stanford.edu.edu>
2020-09-24 17:13:53 -0500

    Added configurations fnd scripts for the "large" cluster

---
+++ instance/large/env.sh
@@ -0,0 +1,48 @@
+# Site-specific configuration
+# ===========================
+
+# The latest version of the Qserv container
+BRANCH=tickets_DM-26609
+
+# Data directory location on docker host, optional
+HOST_DATA_DIR=/qserv/qserv-prod/data
+
+# Log directory location on docker host, optional
+HOST_LOG_DIR=/qserv/qserv-prod/log
+
+# Temporary directory location on docker host, optional
+HOST_TMP_DIR=/qserv/qserv-prod/tmp
+
+# The home directory of the master container for dumping core files
+HOST_MASTER_HOME_DIR=/qserv/qserv-prod/master
+
+# ulimit memory lock setting, in bytes, optional
+ULIMIT_MEMLOCK=10737418240
+
+# Master id
+MASTER_ID=01
+
+# Optional, default to <HOSTNAME_FORMAT>
+MASTER_FORMAT="lsst-qserv-master%02g"
+
+# Optional, default to <SSH_HOSTNAME_FORMAT>
+# then $MASTER"
+SSH_MASTER_FORMAT="qserv-master${MASTER_ID}"
+
+# Format for all node's hostname
+HOSTNAME_FORMAT="lsst-qserv-db%02g"
+
+# Optional, format for node's ssh name
+# Used at NCSA
+SSH_HOSTNAME_FORMAT="qserv-db%02g"
+
+# Workers range
+WORKER_FIRST_ID=01
+WORKER_LAST_ID=30
+
+# Advanced configuration
+# ======================
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+. "${DIR}/common.sh"
+
---
+++ instance/large/replication/secrets/README.md
@@ -0,0 +1,1 @@
+../../../../scripts/replication/secrets/README.md
