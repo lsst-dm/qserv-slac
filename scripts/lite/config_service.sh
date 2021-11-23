24e768228cdcb4a8750c09189f7f133e9019c0bd
Igor Gaponenko <gapon@slac.stanford.edu>
2021-11-22 18:36:42 -0600

    Fixed a bug in the service configuration script

--- scripts/lite/config_service.sh
+++ scripts/lite/config_service.sh
@@ -21,6 +21,7 @@ if [ -n "${CZAR_XROOTD}" ]; then
 fi
 if [ -n "${CZAR_PROXY}" ]; then
     sudo -u qserv vim ${QSERV_BASE_DIR}/config-etc/my-proxy.cnf
+    sudo -u qserv vim ${QSERV_BASE_DIR}/config-etc/qserv-czar.cnf
 fi
 if [ -n "${WORKER_DB}" ]; then
     >&2 echo "error: worker's mariadb service can't be reconfigured by this tool"
